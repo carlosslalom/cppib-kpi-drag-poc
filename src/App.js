162219b3ff39aaa21630a8fe3b33781f6626aafc
Mediha Munim <mmunim@husky.ca>
2020-01-21 09:50:03 -0500

    create basic dropzone

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback} from 'react';
+import {useDropzone} from 'react-dropzone';
 import './App.css';
 
 function App() {
+  const onDrop = useCallback(acceptedFiles => {
+    // do something with the files
+    console.log(acceptedFiles);
+  }, []);
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    onDrop,
+    multiple: false,
+    maxSize: 500000
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+          <p>Drop the file here...</p> :
+          <p>Drag and drop a file here, or click to select a file</p>
+        }
+      </div>
     </div>
   );
 }
